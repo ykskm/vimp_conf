786f2d20a07d70e7faac1c6aaab495eaae3cc70b
ykskm <ykskm9@gmail.com>
2013-01-21 23:47:12 +0900

    Update plugins

--- plugin/subscldr.js
+++ plugin/subscldr.js
@@ -126,10 +126,9 @@ liberator.plugins.subscldr = (function() {
       var subscribeInfo;
 
       var uri = target || endpoint + buffer.URL;
-      liberator.log(uri);
 
       var req = new libly.Request(uri, null, {asynchronous: false});
-      req.addEventListener("onSuccess", function(res) {
+      req.addEventListener("success", function(res) {
         liberator.log(res.responseText);
         res.getHTMLDocument();
         if (isLoginForm(res.doc)) throw "Please login to LDR to subscribe the feed.";
@@ -161,11 +160,11 @@ liberator.plugins.subscldr = (function() {
           postBody: postBody
         }
       );
-      req.addEventListener("onSuccess", function(data) {
+      req.addEventListener("success", function(data) {
         liberator.log("Post status: " + data.responseText);
         liberator.echo("Subscribe feed succeed.");
       });
-      req.addEventListener("onFailure", function(data) {
+      req.addEventListener("failure", function(data) {
         liberator.log("POST FAILURE: " + data.responseText);
         liberator.echoerr("POST FAILURE: " + data.statusText);
       });
